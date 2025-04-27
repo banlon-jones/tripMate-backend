3596eb2b7089d182298ce2c738f465b381293701
jones <banlonjones@urpharm.com>
2025-04-27 15:23:19 +0100

    initialize backend with Express, MongoDB, and dotenv; add .gitignore and package.json

---
+++ index.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import express from "express";
+import taskRouter from "./src/routes/taskRoutes.js";
+import cors from "cors";
+
+dotenv.config({ path: '.env' });
+
+mongoose.connect(`${process.env.DATABASE_URI}`);
+
+try {
+  // connecting to MongoDB
+  mongoose.connection.on("connected", () => {
+    console.log("Connected to MongoDB");
+  });
+} catch (error) {
+  console.error(`Error connecting to MongoDB: ${error}`);
+}
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+const corsOptions = {
+  origin: "*", // Allow requests from this origin
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed HTTP methods
+  credentials: true, // Allow cookies if needed
+};
+
+app.use(cors(corsOptions));
+
+app.use("/api/todos", taskRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
+
